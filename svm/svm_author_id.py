39866838fd3b214502324151bd43304a99253f19
Alashmony <a.alashmony@outlook.com>
2021-05-22 18:13:16 +0200

    add first changes including first 4 projects

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+#clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C = 10000)
 
+print(clf.kernel)
+t0 = time()
+#to_cut = int(len(features_train)/100)
+#print(len(features_train))
+#print(to_cut)
+#features_train = features_train[:to_cut]
+#labels_train = labels_train[:to_cut]
+#clf.fit(features_train, labels_train)
+clf.fit(features_train, labels_train)
+#print("Done fitting")
+print("training time:", round(time()-t0, 3), "s")
+pred = clf.predict(features_test)
+#print("Done pred")
+acc_s = accuracy_score(labels_test,pred,normalize=True)
+print(acc_s)
 #########################################################
 
 
